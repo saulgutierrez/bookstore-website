eaf887b3b874f56ad509f7190c0bd0325a730e62
Saúl Gutiérrez <sauladai@hotmail.com>
2023-07-20 03:26:06 -0600

    Proyecto finalizado

--- index.js
+++ index.js
@@ -12,4 +12,30 @@ var swiper = new Swiper('.swiper', {
         delay: 5000,
         disableOnInteraction: true,
     },
+    breakpoints: {
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 0,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+        },
+        600: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+    },
 });
+
+/* Funcion para activar el menu responsive */
+function toggleMenu() {
+    var menuToggle = document.querySelector('.menuToggle');
+    var nav = document.querySelector('.nav');
+    menuToggle.classList.toggle('active');
+    nav.classList.toggle('active');
+}
